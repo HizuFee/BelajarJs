dfb29359d9b83fb2115d163cb2f2dfb75b5e2a03
Sandy Triananda <legangamerz@gmail.com>
2024-01-16 16:47:21 +0700

    Add files via upload

---
+++ javascript/input_variable_dan_menampilkan/main.js
@@ -0,0 +1,8 @@
+
+//let nama = 'sandy'         tipe string
+//let usia = 16              tipe integer number
+//let TinggiBadan = 150.5    tipe double float (koma (,) di javascript menggunakan titik (.))
+
+
+let usia = prompt ('berapa usia anda')
+alert('usia anda adalah ' + usia)
---
+++ javascript/var/main.js
@@ -0,0 +1,15 @@
+//macam macam variable
+
+// var a = 0    (nilai var bisadi ubah)
+
+// const b= 0 (nilai const tidak dapat di ubah)( menyebabkan eror ketika di ubah Uncaught TypeError: Assignment to constant variable.at main.js:7:6)
+
+// let c= 0 (nilai let bisa di ubah)
+
+//pengetahuan dasar (code di eksekusi dari atas ke bawah)
+
+let usia = 18
+usia = 20
+
+console.log('usia kamu adalah '+ usia)
+alert ('usia kamu adalah '+ usia)
